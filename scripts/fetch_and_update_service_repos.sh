e371b79b88cfbfcec8f2e655d832c1fd41e787d8
cam <cam@localhost.localdomain>
2021-08-13 02:05:01 -0400

    abstracting tasks into categories

---
+++ scripts/fetch_and_update_service_repos.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Fetch latest version(s) of each of the comprising repos that make up the overarching-microserivce, 
+# build/compress size, and finally group together as a means of simplifing deployment across multiple applications.
